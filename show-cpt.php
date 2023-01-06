1d570fa8dc8c80b8e245fa9c56ed7b7515190f61
Trung Tran <tranngoctrung.ictu@gmail.com>
2023-01-06 16:50:00 +0700

    Update show-cpt.php

--- show-cpt.php
+++ show-cpt.php
@@ -28,3 +28,41 @@ function query_tax_post_type($query)
 	return $query;
 }
 add_filter('pre_get_posts', 'query_tax_post_type');
+
+// Get list child of post type
+function categories_child($atts = array())
+{
+	$args = array(
+		'taxonomy' => $atts['taxonomy'],
+		'orderby' => 'id',
+		'order'   => 'ASC',
+		'hide_empty' => false,
+	);
+	$categories = get_categories($args);
+	$taxonomy = get_queried_object()->taxonomy;
+
+	$isCPT = get_queried_object()->name == $atts['post_type'];
+	$isCatCPT = $taxonomy == $atts['taxonomy'];
+
+	$className = "current-cat";
+	$class_current = $isCPT ? $className : "";
+
+	$current_id = get_queried_object()->term_id;
+
+	$output = '';
+	$output .= '<ul class="list-categories-child">';
+	$output .= '<li class="all ' . $class_current . '"><a href="' . get_post_type_archive_link($atts['post_type']) . '">すべて</a></li>';
+	$class_current = $isCatCPT ? $className : "";
+	foreach ($categories as $category) {
+		$class_current = '';
+		if ($category) {
+			if ($current_id == $category->term_id) $class_current .= $className;
+			$output .= '<li class="category cat-item cat-item-' . $category->term_id . ' ' . $class_current . '"><a href="' . get_category_link($category->term_id) . '" title="' . $category->name . '" ' . '><span>' . $category->name . '</span></a>';
+		}
+	}
+	$output .= '</li>';
+	$output .= '</ul>';
+
+	return $output;
+}
+add_shortcode('categories_child', 'categories_child');
